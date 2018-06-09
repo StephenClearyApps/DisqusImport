dbd3737cb22ce6f64ae2165e36f565c2d8235060
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-06-09 10:27:07 -0400

    Define StaticmanPostId as a universal solution.

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -25,8 +26,9 @@ namespace DisqusImport
                 foreach (var threadedPosts in data.post.GroupBy(x => x.thread.id))
                 {
                     // Use `thread/link` from XML as our Staticman `post_id`
-                    var uri = new Uri(threadLookup[threadedPosts.Key]);
-                    var staticmanPostId = Path.ChangeExtension(uri.AbsolutePath, "").Replace('/', '_').Trim('_');
+                    var threadLink = threadLookup[threadedPosts.Key];
+                    var uri = new Uri(threadLink);
+                    var staticmanPostId = GitFilename(Path.ChangeExtension(uri.AbsolutePath, "")).Trim('_');
 
                     foreach (var post in threadedPosts.Where(x => !x.isDeleted))
                     {
@@ -56,6 +58,55 @@ namespace DisqusImport
                 Message = post.message,
                 Date = post.createdAt,
             };
+        }
+
+        private static readonly Regex extension = new Regex(@"\.[A-Za-z0-9_]{3}$");
+        private static readonly Encoding Utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+        private static string StaticmanPostId(string url)
+        {
+            // 1) If the URL starts with http://, replace with https://
+            if (url.StartsWith("http://"))
+                url = "https://" + url.Substring(7);
+
+            // 2) Take the URL path (not including scheme and domain).
+            var uri = new Uri(url);
+            var path = uri.AbsolutePath;
+
+            // 3) Path: If the path ends with .html or any three-character [A-Za-z0-9_] extension, strip it.
+            if (path.EndsWith(".html"))
+                path = path.Substring(0, path.Length - 5);
+            else if (extension.IsMatch(path))
+                path = path.Substring(0, path.Length - 4);
+
+            // 4) Path: For each UTF-16 code unit, if it's not in the whitelist [A-Za-z0-9-_;.~()], then replace it with _.
+            path = GitFilename(path);
+
+            // 5) Path: Trim leading and trailing _ characters.
+            path = path.Trim('_');
+
+            // 6) Calculate the V3 URL GUID for the URL.
+            var guid = Nito.Guids.GuidFactory.CreateMd5(Nito.Guids.GuidNamespaces.Url, Utf8.GetBytes(url));
+
+            // 7) Combine the path and lowercase-hyphenated GUID, separated by '-'.
+            return path + "-" + guid.ToString("D");
+
+        }
+
+        private static string GitFilename(string input)
+        {
+            // Git has a lot of problems with non-ASCII characters.
+            var sb = new StringBuilder(input.Length);
+            foreach (var ch in input)
+            {
+                if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || ch == '-' ||
+                    ch == '_' || ch == ';' || ch == '.' || ch == '~' || ch == '(' || ch == ')')
+                    sb.Append(ch);
+                else
+                    sb.Append('_');
+            }
+
+            return sb.ToString();
         }
 
         private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
---
+++ src/StringExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DisqusImport
+{
+    public static class StringExtensions
+    {
+        public static TextElementEnumerable TextElements(this string source) => new TextElementEnumerable(source);
+
+        public struct TextElementEnumerable: IEnumerable<string>
+        {
+            private readonly string _source;
+
+            public TextElementEnumerable(string source)
+            {
+                _source = source;
+            }
+
+            public TextElementEnumerator GetEnumerator() => new TextElementEnumerator(StringInfo.GetTextElementEnumerator(_source));
+
+            IEnumerator<string> IEnumerable<string>.GetEnumerator() => GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        public struct TextElementEnumerator: IEnumerator<string>
+        {
+            private readonly System.Globalization.TextElementEnumerator _source;
+
+            public TextElementEnumerator(System.Globalization.TextElementEnumerator source)
+            {
+                _source = source;
+            }
+
+            public bool MoveNext() => _source.MoveNext();
+
+            public void Reset() => _source.Reset();
+
+            public string Current => (string)_source.Current;
+
+            object IEnumerator.Current => _source.Current;
+
+            public void Dispose() { }
+        }
+    }
+}
